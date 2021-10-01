d98f88cdd489572c81a2df444bb35e1e3c12d7b7
TriangleBear <kakanini31@gmail.com>
2021-10-01 18:13:51 +0800

    Update testConnection.java
    Connection testing

--- Cinema_Ticketing_System/src/testConnection.java
+++ Cinema_Ticketing_System/src/testConnection.java
@@ -11,7 +11,7 @@ import java.sql.*;
  * @author ACER
  */
 public class testConnection {
-
+//this is a connection testing
     public static void main(String args[]) {
         try{  
             Class.forName("com.mysql.cj.jdbc.Driver");  
