e29474edda38df36af7de3b23111959bb053afaa
TriangleBear <kakanini31@gmail.com>
2021-10-01 18:05:42 +0800

    Create testConnection.java
    if it doesn't run, means that you didn't used the connector, and dependencies
    
    Pleasee mention me in discord to show you the fix

---
+++ Cinema_Ticketing_System/src/testConnection.java
@@ -0,0 +1,26 @@
+
+import java.sql.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ACER
+ */
+public class testConnection {
+
+    public static void main(String args[]) {
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql","root","2001");  
+            Statement stmt = conn.createStatement();  
+            ResultSet rs = stmt.executeQuery("select * from db");    
+            conn.close();  
+        }catch(Exception ex){
+            System.out.println(ex);
+        } 
+    }
+}
