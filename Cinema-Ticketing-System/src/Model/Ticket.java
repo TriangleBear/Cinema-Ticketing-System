1413f9f315fb201ccb67e2788020935702a05a55
TriangleBear <kakanini31@gmail.com>
2021-10-10 17:51:56 +0800

    Update Ticket.java
    Changed to static

--- Cinema-Ticketing-System/src/Model/Ticket.java
+++ Cinema-Ticketing-System/src/Model/Ticket.java
@@ -2,16 +2,18 @@ package Model;
 import java.util.Observable;
 public class Ticket extends Observable
 {
-    private static String ticketNumber;
+    private String ticketNumber;
+    private static String movie;
     private static String cinemaNumber;
     private static String movieTime;
     private static String dateNow;
-    private static  String cinemaSeat;
+    private static String cinemaSeat;
     private int price = 250;
 
-    public Ticket(String tNumber, String cinema, String Seat, String Time, String Date, int price){
+    public Ticket(String tNumber, String movie, String cinema, String Seat, String Time, String Date, int price){
         this.ticketNumber = tNumber;
         this.cinemaNumber = cinema;
+        this.movie = movie;
         this.cinemaSeat = Seat;
         this.movieTime = Time;
         this.dateNow = Date;
@@ -21,14 +23,17 @@ public class Ticket extends Observable
         setChanged();
         notifyObservers();
     }
-    public static String getTicket(){
+    public String getTicket(){
         return ticketNumber;
     }
-    public static String getSeat(){
-        return cinemaSeat;
+    public static String getMovie() {
+    	return movie;
     }
     public static String getCinema(){
         return cinemaNumber;
+    }
+    public static String getSeat(){
+        return cinemaSeat;
     }
     public static String getTime(){
         return movieTime;
@@ -43,14 +48,19 @@ public class Ticket extends Observable
         this.ticketNumber = tNumber;
         ticketChanged();
     }
+    public void setMovie(String movie) {
+    	this.movie = movie;
+    	ticketChanged();
+    }
+    public void setCinema(String cinema){
+        this.cinemaNumber = cinema;
+        ticketChanged();
+    }
     public void setSeat(String Seat){
     this.cinemaSeat = Seat;
     ticketChanged();
     }
-    public void setCinema(String cinema){
-    this.cinemaNumber = cinema;
-    ticketChanged();
-    }
+    
     public void setTime(String Time){
     this.movieTime = Time;
     ticketChanged();
